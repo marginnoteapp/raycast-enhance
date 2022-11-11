f9a264fce80bf2df0eee7d26485ceb468a4f6312
ourongxing <orongxing@gmail.com>
2022-11-11 16:47:44 +0800

    release: v1.0.0

--- README.md
+++ README.md
@@ -4,14 +4,14 @@
 </p>
 
 <p align="center">
-  <a href="https://github.com/marginnoteapp/raycast-enhance/network/members"><img src="https://img.shields.io/github/forks/marginnoteapp/raycast-enhance.svg?style=flat" alt="forks"></a>
-  <a href="https://github.com/marginnoteapp/raycast-enhance/stargazers"><img src="https://img.shields.io/github/stars/marginnoteapp/raycast-enhance.svg?style=flat" alt="stars"></a>
-  <a href="https://github.com/marginnoteapp/raycast-enhance/blob/main/package.json"><img src="https://img.shields.io/badge/version-v0.9.0-orange" alt="version"></a>
-  <a href="https://github.com/marginnoteapp/raycast-enhance/blob/main/LICENSE"><img src="https://img.shields.io/badge/license-MIT-green" alt="license"></a>
+  <a href="https://github.com/marginnoteapp/$2/network/members"><img src="https://img.shields.io/github/forks/marginnoteapp/$2.svg?style=flat" alt="forks"></a>
+  <a href="https://github.com/marginnoteapp/$2/stargazers"><img src="https://img.shields.io/github/stars/marginnoteapp/$2.svg?style=flat" alt="stars"></a>
+  <a href="https://github.com/marginnoteapp/$2/blob/main/package.json"><img src="https://img.shields.io/badge/version-v0.9.0-orange" alt="version"></a>
+  <a href="https://github.com/marginnoteapp/$2/blob/main/LICENSE"><img src="https://img.shields.io/badge/license-MIT-green" alt="license"></a>
 </p>
 
 > No Icon, No Logo
 
 ## License
 
-[MIT](https://github.com/marginnoteapp/raycast-enhance/blob/main/LICENSE)
+<a href="https://github.com/marginnoteapp/$2/blob/main/LICENSE">MIT</a> © <a href="https://github.com/marginnoteapp"><img src="https://testmnbbs.oss-cn-zhangjiakou.aliyuncs.com/pic/mn.png?x-oss-process=base_webp" alt="MarginNote" width="80"></a>
--- main.js
+++ main.js
@@ -1,72 +1,68 @@
-;(function () {
-  const Addon = {
-    name: "Raycast Enhance",
-    key: "raycastenhance"
-  }
-  const console = {
-    log(obj) {
-      JSB.log(`${Addon.key} %@`, obj)
+/**
+ * MIT License
+ * Copyright © 2022 MarginNote
+ * Github: https://github.com/marginnoteapp/raycast-enhance
+ * Welcom to contribute to this project!
+ */
+
+try {
+  ;(() => {
+    const Addon = {
+      name: "Raycast Enhance",
+      key: "raycastenhance"
     }
-  }
-  const isMac = Application.sharedInstance().osType === 2
-  function dateFormat(date, fmt = "YYYY-mm-dd HH:MM") {
-    let ret
-    const opt = {
-      "Y+": date.getFullYear().toString(),
-      "m+": (date.getMonth() + 1).toString(),
-      "d+": date.getDate().toString(),
-      "H+": date.getHours().toString(),
-      "M+": date.getMinutes().toString(),
-      "S+": date.getSeconds().toString() // second
-    }
-    Object.entries(opt).forEach(([k, v]) => {
-      ret = new RegExp("(" + k + ")").exec(fmt)
-      if (ret) {
-        fmt = fmt.replace(
-          ret[1],
-          ret[1].length == 1 ? v : v.padStart(ret[1].length, "0")
-        )
-      }
-    })
-    return fmt
-  }
-  JSB.newAddon = (mainPath) => {
-    function fetchNotebook() {
-      try {
-        if (!isMac) return
-        const notebooks = Database.sharedInstance().allNotebooks().reduce((acc, k) => {
-          if (
-            k.flags === 2 ||
-            k.flags === 3
-          ) {
-            acc.push({
-              id: k.topicId,
-              title: k.title,
-              lastVisit: dateFormat(k.lastVisit, "YYYY-mm-dd HH:MM"),
-              type: k.flags === 2 ? 1 : 2
-            })
-          }
-          return acc
-        }, [])
-        NSData.dataWithStringEncoding(
-          JSON.stringify(notebooks),
-          4
-        ).writeToFileAtomically(mainPath + "/notebook.json", false)
-      } catch (err) {
-        console.log(err)
+    const console = {
+      log(obj, suffix = "normal") {
+        JSB.log(`${Addon.key}-${suffix} %@`, obj)
       }
     }
-    return JSB.defineClass(
-      Addon.name + ": JSExtension",
-      {
-        sceneDidDisconnect() {
-          fetchNotebook()
-        },
-        sceneWillResignActive() {
-          fetchNotebook()
+    const isMac = Application.sharedInstance().osType === 2
+    JSB.newAddon = mainPath => {
+      function fetchNotebook() {
+        try {
+          if (!isMac) return
+          const notebooks = Database.sharedInstance()
+            .allNotebooks()
+            .reduce((acc, k) => {
+              if (k.flags === 2 || k.flags === 3) {
+                acc.push({
+                  id: k.topicId,
+                  title: k.title,
+                  lastVisit: k.lastVisit.getTime(),
+                  type: k.flags === 2 ? 1 : 2
+                })
+              }
+              return acc
+            }, [])
+          NSData.dataWithStringEncoding(
+            JSON.stringify(notebooks),
+            4
+          ).writeToFileAtomically(mainPath + "/notebooks.json", false)
+        } catch (err) {
+          console.log(err)
         }
-      },
-      {}
-    )
-  }
-})()
+      }
+      let isInNotebook = false
+      return JSB.defineClass(
+        Addon.name + ": JSExtension",
+        {
+          sceneDidDisconnect() {
+            fetchNotebook()
+          },
+          sceneWillResignActive() {
+            isInNotebook && fetchNotebook()
+          },
+          notebookWillOpen() {
+            isInNotebook = true
+          },
+          notebookWillClose() {
+            isInNotebook = false
+          }
+        },
+        {}
+      )
+    }
+  })()
+} catch (err) {
+  JSB.log("raycastenhance-error %@", String(err))
+}
